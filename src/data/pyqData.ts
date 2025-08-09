f68b0b1a972244c3668a74647b4319b86e99dc59
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-08-10 02:57:25 +0530

    Updated pyqData.ts

--- src/data/pyqData.ts
+++ src/data/pyqData.ts
@@ -464,7 +464,7 @@ export const pyqSubjects: PYQSubject[] = [
     id: 'cse-s1-math1',
     name: 'Engineering Mathematics-1',
     code: '1FY2-01',
-    branchId: 'ee',
+    branchId: {'ee','ece'},
     semester: 1,
     credits: 4
   },
