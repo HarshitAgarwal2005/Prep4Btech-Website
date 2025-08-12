3b102c8c7a86f7775de16b085d877c94e682c884
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-08-13 01:17:34 +0530

    Updated pyqData.ts

--- src/data/pyqData.ts
+++ src/data/pyqData.ts
@@ -1,31 +1,31 @@
-import { Course, Branch, PYQSubject, PYQPaper } from '../types';
+import { Branch, PYQSubject, PYQPaper } from '../types';
 
-export const courses: Course[] = [
-  {
-    id: 'btech',
-    name: 'Bachelor of Technology',
-    code: 'B.Tech',
-    description: 'Undergraduate engineering program'
-  },
-  {
-    id: 'bca',
-    name: 'Bachelor of Computer Applications',
-    code: 'BCA',
-    description: 'Undergraduate computer applications program'
-  },
-  {
-    id: 'mba',
-    name: 'Master of Business Administration',
-    code: 'MBA',
-    description: 'Postgraduate business administration program'
-  },
-  {
-    id: 'mca',
-    name: 'Master of Computer Applications',
-    code: 'MCA',
-    description: 'Postgraduate computer applications program'
-  }
-];
+// export const courses: Course[] = [
+//   {
+//     id: 'btech',
+//     name: 'Bachelor of Technology',
+//     code: 'B.Tech',
+//     description: 'Undergraduate engineering program'
+//   },
+//   {
+//     id: 'bca',
+//     name: 'Bachelor of Computer Applications',
+//     code: 'BCA',
+//     description: 'Undergraduate computer applications program'
+//   },
+//   {
+//     id: 'mba',
+//     name: 'Master of Business Administration',
+//     code: 'MBA',
+//     description: 'Postgraduate business administration program'
+//   },
+//   {
+//     id: 'mca',
+//     name: 'Master of Computer Applications',
+//     code: 'MCA',
+//     description: 'Postgraduate computer applications program'
+//   }
+// ];
 
 export const branches: Branch[] = [
   // B.Tech Branches
