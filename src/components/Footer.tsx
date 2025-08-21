5fe952895f0028d1f0d73ed71b2cfb629e7dd547
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-08-21 16:24:59 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -77,8 +77,7 @@ const Footer: React.FC = () => {
   const handleSubscribe = (e : React.FormEvent) => {
     e.preventDefault();
 
-    emailjs
-      .send(
+    emailjs.send(
         "service_email",    // 🔹 Service ID from EmailJS
         "template_m60p9tg",   // 🔹 Template ID from EmailJS
         { subscriber_email: email }, // data you pass to template
