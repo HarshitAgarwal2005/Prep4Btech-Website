ed6381db9c0de98de67226c9f19ea78c17beac7e
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-08-21 16:40:53 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,11 +76,11 @@ const Footer: React.FC = () => {
     e.preventDefault();
     if (email.trim()) {
        emailjs.send(
-     //    "service_email",    // 🔹 Service ID from EmailJS
-     //    "template_m60p9tg",   // 🔹 Template ID from EmailJS
-     //    { subscriber_email: email }, // data you pass to template
-     //    "ykUUgVhuU-DLIy3IX"     // 🔹 Public Key from EmailJS
-     //  ) 
+        "service_email",    // 🔹 Service ID from EmailJS
+        "template_m60p9tg",   // 🔹 Template ID from EmailJS
+        { subscriber_email: email }, // data you pass to template
+        "ykUUgVhuU-DLIy3IX"     // 🔹 Public Key from EmailJS
+      ) 
       setIsSubscribed(true);
       const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
       setMotivationalQuote(randomQuote);
