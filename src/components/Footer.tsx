5210bfdb104f9ce94b034644af2dc85019b0f727
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-08-25 14:53:08 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,9 +21,9 @@ import { Link } from 'react-router-dom';
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
   const [visitCount, setVisitCount] = useState(1000);
-  // const [email, setEmail] = useState('');
-  // const [isSubscribed, setIsSubscribed] = useState(false);
-  // const [motivationalQuote, setMotivationalQuote] = useState('');
+  const [email, setEmail] = useState('');
+  const [isSubscribed, setIsSubscribed] = useState(true);
+  const [motivationalQuote, setMotivationalQuote] = useState('');
   const [email, setEmail] = useState("");
   const [isSubscribed, setIsSubscribed] = useState(false);
   const [motivationalQuote, setMotivationalQuote] = useState("");
