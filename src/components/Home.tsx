654149ec4e1dbb0ce0f951b764caae0bf94e4a24
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-08-30 17:27:15 +0530

    Updated Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -180,7 +180,7 @@ const Home: React.FC = () => {
       </div>
 
       {/* How to Use Section */}
-      <div className="py-16 sm:py-24 relative">
+      <div className="py-10 sm:py-24 relative">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
             <h2 className="text-3xl sm:text-4xl font-bold text-gray-900 dark:text-white mb-4">
