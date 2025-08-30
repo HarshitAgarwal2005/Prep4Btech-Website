8383d6d9553a5dd3631b1e2b8ad8d6d8a8257ec1
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-08-30 15:55:42 +0530

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -205,7 +205,7 @@ const About: React.FC = () => {
                 </div>
               </div>
             </div>
-          </div>
+          
                
                 
                 
