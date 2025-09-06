8b37733f9ef35b086efdf97ac1355618522232a8
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-06 11:15:20 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -1625,7 +1625,7 @@ export const contentItems: ContentItem[] = [
     subjectId: 'mpws-s1-cse',
     fileSize: '1.9 MB',
     uploadDate: '2024-01-16',
-    viewUrl: 'https://drive.google.com/file/d/1Ofj5-E--2L95DsM4j-XY-AFrq0fY_C8F/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1Tzpkz3FgGiG-tUYy7nB_j07X4Tu-C1aD/view?usp=drive_link'
   },
   {
     id: 'mpws-theory-6-cse',
