f9b0e91d9e98ec7b3860c675663c8668fbb9c9fe
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-06 10:46:30 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -1544,7 +1544,6 @@ export const contentItems: ContentItem[] = [
     subjectId: 'camd-s1-cse',
     fileSize: '1.9 MB',
     uploadDate: '2024-01-16',
-    // viewUrl: 'https://example.com/camd-manual'
     viewUrl: 'https://youtube.com/playlist?list=PLDN15nk5uLiBpnIOK5r3KXdfFOVzGHJSt&si=yRjEILVa75JwfS4T'
   },
    {
