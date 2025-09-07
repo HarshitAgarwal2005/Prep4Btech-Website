dd2bae0ae4b98ce7e6d8d23e54981837514c2639
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-07 23:57:51 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -1706,7 +1706,7 @@ export const contentItems: ContentItem[] = [
     subjectId: 'mpws-s1-cse',
     fileSize: '1.9 MB',
     uploadDate: '2024-01-16',
-    viewUrl: 'https://drive.google.com/file/d/1Ofj5-E--2L95DsM4j-XY-AFrq0fY_C8F/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1DL7yJsV54kDuyIgq21FpVr8cdv1Lrd0p/view?usp=drive_link'
   },
   {
     id: 'mpws-lab-7-cse',
