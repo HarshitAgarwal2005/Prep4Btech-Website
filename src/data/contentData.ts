e1187b7bb151ab1e43f6fea86ff52d4a5d401bfb
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-12 01:16:56 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2415,6 +2415,16 @@ viewUrl:'https://drive.google.com/file/d/1_axNV1orwJlW_P4EgARKypzfsEt2R-H6/view?
     uploadDate: '2024-02-01',
     viewUrl: 'https://drive.google.com/file/d/19g8gx49P123jlBjfuCML7vCJpX6bOuiu/view?usp=sharing'
   },
+  {
+    id: 'hv-theory-7-cse',
+    title: 'Important Questions',
+    description: 'All Units Important Ques.',
+    type: 'theory',
+    subjectId: 'hv-s2-cse',
+    fileSize: '24.2 MB',
+    uploadDate: '2024-02-01',
+    viewUrl: 'https://drive.google.com/file/d/1mhVk9tLesks5jJyl1f--R5FtkgSJtvHz/view?usp=sharing'
+  },
   //BASIC CIVIL ENGINEERING
   {
     id: 'civil-theory-1-cse',
