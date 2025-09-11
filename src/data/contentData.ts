054ccf5f031094417942726ed9459193d3413919
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-11 23:27:50 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2691,7 +2691,7 @@ export const contentItems: ContentItem[] = [
     subjectId: 'de-s3-cse',
     fileSize: '4.2 MB',
     uploadDate: '2024-02-15',
-    viewUrl: 'https://drive.google.com/file/d/1oJ5d3jVw7CWBpwPsq-2hxYQKPR60zr3W/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/13dk3kn7jnWxhDjsZsfC9gGAMdSxzYrfa/view?usp=drive_link'
   },
   {
     id: 'de-books-4-cse',
