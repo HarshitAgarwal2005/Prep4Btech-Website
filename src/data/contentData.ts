f615dc7f57a589942d006136b611b5a7bc6bbb8b
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-12 00:02:44 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2942,7 +2942,7 @@ export const contentItems: ContentItem[] = [
     subjectId: 'mefa-s3-cse',
     fileSize: '1.3 MB',
     uploadDate: '2024-02-25',
-    viewUrl: 'https://drive.google.com/file/d/1NlKUV-T_b4ERicziOFs9qAOnx9D3usZC/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1Z10Fg8QHx5txpzfYOAk2nGXeVCubLw-0/view?usp=drive_link'
   },
   {
     id: 'mefa-assignments-5-cse',
