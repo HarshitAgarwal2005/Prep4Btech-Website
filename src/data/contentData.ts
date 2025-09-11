a0620da2c0797e1166fa7b8cf5ea375458cdd273
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-11 23:06:26 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2538,7 +2538,7 @@ export const contentItems: ContentItem[] = [
     subjectId: 'aem-s3-cse',
     fileSize: '4.2 MB',
     uploadDate: '2024-02-15',
-    viewUrl: 'https://drive.google.com/file/d/1mxOsC7ZM1WDxaBYEk57BZu7eYlo-fQek/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1GoJn6UEnJzLH2K9Xo5HgW-O-ACbtVluB/view?usp=drive_link'
   },
   {
     id: 'aem-notes-3-cse',
@@ -2570,16 +2570,16 @@ export const contentItems: ContentItem[] = [
     uploadDate: '2024-02-15',
     viewUrl: 'https://drive.google.com/file/d/1mxOsC7ZM1WDxaBYEk57BZu7eYlo-fQek/view?usp=drive_link'
   },
-  {
-    id: 'aem-notes-1-cse',
-    title: 'unit -1',
-    description: 'Advanced Engineering Mathematics',
-    type: 'notes',
-    subjectId: 'aem-s3-cse',
-    fileSize: '4.2 MB',
-    uploadDate: '2024-02-15',
-    viewUrl: 'https://drive.google.com/file/d/1mxOsC7ZM1WDxaBYEk57BZu7eYlo-fQek/view?usp=drive_link'
-  },
+  // {
+  //   id: 'aem-notes-1-cse',
+  //   title: 'unit -1',
+  //   description: 'Advanced Engineering Mathematics',
+  //   type: 'notes',
+  //   subjectId: 'aem-s3-cse',
+  //   fileSize: '4.2 MB',
+  //   uploadDate: '2024-02-15',
+  //   viewUrl: 'https://drive.google.com/file/d/1mxOsC7ZM1WDxaBYEk57BZu7eYlo-fQek/view?usp=drive_link'
+  // },
   
   // Data Structures & Algorithms Content (for CSE 3rd SEM)
   {
