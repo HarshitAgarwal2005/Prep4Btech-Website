8bb3a4d8c53385edac0fae639d2d89114491fee1
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-12 00:12:54 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2971,7 +2971,7 @@ export const contentItems: ContentItem[] = [
     title: 'OJECT ORIENTED PROGRAMMING UNIT-1',
     description: 'OJECT ORIENTED PROGRAMMING ',
     type: 'theory',
-    subjectId: 'oops-s3-cse',
+    subjectId: 'oops-s3-cse', 
     fileSize: '1.3 MB',
     uploadDate: '2024-02-25',
     viewUrl: 'https://drive.google.com/file/d/1ZY3rIrkdDTtCryxJKxuu7hlWXyjQZv0g/view?usp=drive_link'
