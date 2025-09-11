9cc2331023751e804eed3a8802855ffe2d6acd00
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-12 02:29:22 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2554,7 +2554,7 @@ viewUrl:'https://drive.google.com/file/d/1_axNV1orwJlW_P4EgARKypzfsEt2R-H6/view?
     subjectId: 'civil-s2-cse',
     fileSize: '4.2 MB',
     uploadDate: '2024-02-15',
-    viewUrl: 'https://drive.google.com/file/d/19y4z3pVWg_7cM9LETYEzm2bncCweG9fV/view?usp=sharing'
+    viewUrl: 'https://drive.google.com/file/d/1L8S6obTJb-ezH7-oYctag4uGjwdId9MG/view?usp=sharing'
   },
   {
     id: 'cviil-books-2-cse',
