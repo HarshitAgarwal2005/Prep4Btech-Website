1df31e804cf7193d1884e9e900dcd2880faa57ec
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-13 00:35:24 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -3371,25 +3371,25 @@ viewUrl:'https://drive.google.com/file/d/1_axNV1orwJlW_P4EgARKypzfsEt2R-H6/view?
   },
 
    {
-    id: 'dbms-theory-2-cse',
-    title: 'UNIT 1',
+    id: 'dbms-theory-3-cse',
+    title: 'UNIT 3',
     description: 'DATABASE MANAGEMENT SYSTEM',
     type: 'theory',
     subjectId: 'dbms-s4-cse',
     fileSize: '1.3 MB',
     uploadDate: '2024-02-25',
-    viewUrl: 'https://drive.google.com/file/d/1aq3BRx_q1qLJN9GV7NbPCL4LEutQIc50/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1DFlHGGyiFq6CwEfpPxCAp9fux74QEl0n/view?usp=drive_link'
   },
 
    {
-    id: 'dbms-theory-2-cse',
-    title: 'UNIT 1',
+    id: 'dbms-theory-4-cse',
+    title: 'UNIT 4',
     description: 'DATABASE MANAGEMENT SYSTEM',
     type: 'theory',
     subjectId: 'dbms-s4-cse',
     fileSize: '1.3 MB',
     uploadDate: '2024-02-25',
-    viewUrl: 'https://drive.google.com/file/d/1aq3BRx_q1qLJN9GV7NbPCL4LEutQIc50/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1Or-DMqtNApOgL18UfvFgR9-QgxolIhbT/view?usp=drive_link'
   },
 
    {
