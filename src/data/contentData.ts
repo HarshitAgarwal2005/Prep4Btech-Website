91b7bc859f2412905234f0e8212d8ced6ce7df59
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-13 00:35:49 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -3393,14 +3393,14 @@ viewUrl:'https://drive.google.com/file/d/1_axNV1orwJlW_P4EgARKypzfsEt2R-H6/view?
   },
 
    {
-    id: 'dbms-theory-2-cse',
-    title: 'UNIT 1',
+    id: 'dbms-theory-5-cse',
+    title: 'UNIT 5',
     description: 'DATABASE MANAGEMENT SYSTEM',
     type: 'theory',
     subjectId: 'dbms-s4-cse',
     fileSize: '1.3 MB',
     uploadDate: '2024-02-25',
-    viewUrl: 'https://drive.google.com/file/d/1aq3BRx_q1qLJN9GV7NbPCL4LEutQIc50/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1C9cHeIeJSojiVBatLvmr4wAme77EwEla/view?usp=drive_link'
   },
 
    {
