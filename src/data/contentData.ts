a71aaf297b2fb3f82113fd03b8b8c824ad51488d
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-13 01:08:56 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2260,7 +2260,7 @@ export const contentItems: ContentItem[] = [
     subjectId: 'math2-s2-cse',
     fileSize: '24.2 MB',
     uploadDate: '2024-02-01',
-    viewUrl: 'https://drive.google.com/file/d/1tE4S0ELp_f3beDnyruGNJqJQZrVq843S/view?usp=sharing'
+    viewUrl: 'https://drive.google.com/file/d/1MuhN84myYkLdBmNMStCcGUhetNEVYW4_/view?usp=drive_link'
   },
   {
     id: 'math2-theory-4-cse',
