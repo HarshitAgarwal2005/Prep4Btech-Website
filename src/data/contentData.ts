f38e293549f57db922ee739c1a41270fcac7c36c
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-13 01:09:27 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -2281,7 +2281,7 @@ export const contentItems: ContentItem[] = [
     subjectId: 'math2-s2-cse',
     fileSize: '24.2 MB',
     uploadDate: '2024-02-01',
-    viewUrl: 'https://drive.google.com/file/d/1tE4S0ELp_f3beDnyruGNJqJQZrVq843S/view?usp=sharing'
+    viewUrl: 'https://drive.google.com/file/d/16aND3qpQddPlTFPqavKwi9fVIqGoJPA3/view?usp=drive_link'
   },
   {
     id: 'math2-videos-1-cse',
