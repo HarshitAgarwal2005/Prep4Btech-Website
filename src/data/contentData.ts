fb2edc0372c297072bcfa1848ab8e5a8a35108a5
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-14 04:28:53 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -4271,7 +4271,7 @@ viewUrl:'https://drive.google.com/file/d/1_axNV1orwJlW_P4EgARKypzfsEt2R-H6/view?
     subjectId: 'itc-s5-cse',
     fileSize: '1.3 MB',
     uploadDate: '2024-02-25',
-    viewUrl: 'https://drive.google.com/file/d/1l6Y6oFOvYiyjvOXPZEYSGytMQ-3jRkq9/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1IgdQGA22sLFZqTupHZBLU19koGiWmfOt/view?usp=drive_link'
   },
    {
     id: 'itc-books-4-cse',
@@ -4281,7 +4281,7 @@ viewUrl:'https://drive.google.com/file/d/1_axNV1orwJlW_P4EgARKypzfsEt2R-H6/view?
     subjectId: 'itc-s5-cse',
     fileSize: '1.3 MB',
     uploadDate: '2024-02-25',
-    viewUrl: 'https://drive.google.com/file/d/1l6Y6oFOvYiyjvOXPZEYSGytMQ-3jRkq9/view?usp=drive_link'
+    viewUrl: 'https://drive.google.com/file/d/1Iv-PrNXmw0zAcwYG6yME9F5pTaB7EfEd/view?usp=drive_link'
   },
   // 4th year
   
