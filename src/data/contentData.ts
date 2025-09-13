766b3250baa1a71c19f71d4f5a8c2bb4647317d2
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-09-14 02:28:22 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -3965,7 +3965,7 @@ viewUrl:'https://drive.google.com/file/d/1_axNV1orwJlW_P4EgARKypzfsEt2R-H6/view?
     subjectId: 'tc-s4-cse',
     fileSize: '1.3 MB',
     uploadDate: '2024-02-25',
-    viewUrl: 'https://docs.google.com/document/d/1tNBmCbDtLGScAcAgX6YWCSOittX7Zn7v/edit?usp=drive_link&ouid=109272455612884300462&rtpof=true&sd=true'
+    viewUrl: 'https://drive.google.com/file/d/1a-OqlMRCpetoQaE1CtwCOrhPil9zSqNt/view?usp=drive_link'
   },
   {
     id: 'tc-theory-3-cse',
