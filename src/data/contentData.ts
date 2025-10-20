fab9fc0a7567cef7444cbccd243f443f3d68fd1e
Hariom <hariom.shivnani23@gmail.com>
2025-10-20 15:57:25 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -3636,7 +3636,7 @@ viewUrl:'https://drive.google.com/file/d/1_axNV1orwJlW_P4EgARKypzfsEt2R-H6/view?
     subjectId: 'dsa-s3-cse',
     fileSize: '4.2 MB',
     uploadDate: '2024-02-15',
-    viewUrl: 'https://docs.google.com/presentation/d/1P67GwVYCTF3jFDhe6tQmQGP-wWQGJ5qG/edit?usp=drive_link&ouid=109272455612884300462&rtpof=true&sd=true'
+    viewUrl: 'https://docs.google.com/presentation/d/1ZY35p2z6xY4QFVeemEwijmGEhYv4t6c6/edit?usp=sharing&ouid=109272455612884300462&rtpof=true&sd=true'
   },
   {
     id: 'dsa-theory-5-cse',
