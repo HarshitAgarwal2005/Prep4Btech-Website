3d2ad392ce0e49c86d6a333006f3a57ad20d7d27
Harshit Agarwal <harshitagarwal25807@gmail.com>
2025-12-12 01:47:07 +0530

    Updated Assignments.tsx

--- src/components/Assignments.tsx
+++ src/components/Assignments.tsx
@@ -164,6 +164,8 @@ const goBack = () => {
   <!DOCTYPE html>
   <html>
   <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>${content.title} - Prep4Btech Content</title>
     <style>
       html, body {
