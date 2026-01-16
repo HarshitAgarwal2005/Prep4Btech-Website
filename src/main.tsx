4c4b5336f23a4d9609eb1e94e88406752cc72777
Hariom <hariom.shivnani23@gmail.com>
2026-01-16 18:45:41 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import './index.css';
 
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* Wrap your App in the Provider */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
