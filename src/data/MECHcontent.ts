0f36d223b1cce93cf6ed315b96168989c35ba1a8
Hariom <hariom.shivnani23@gmail.com>
2026-01-21 01:10:25 +0530

    Updated MECHcontent.ts

---
+++ src/data/MECHcontent.ts
@@ -0,0 +1,16 @@
+import { ContentSubject, ContentItem } from '../types';
+
+export const contentSubjects: ContentSubject[] = [
+  
+];
+export const contentItems: ContentItem[] = [];
+export const branches = [
+  {
+    id: 'mech',
+    name: 'Mechanical Engineering',
+    code: 'MECH',
+    description: 'Mechanical systems and manufacturing',
+    icon: '⚙️',
+    color: 'from-red-500 to-pink-500'
+  },
+];
