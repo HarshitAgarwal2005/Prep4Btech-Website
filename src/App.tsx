de6d63084393cf00d56bcbd6a64fb98fad1eb614
Hariom <hariom.shivnani23@gmail.com>
2026-01-24 00:17:25 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ function App() {
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/privacy-policy" element={<PrivacyPolicy />} />
                 <Route path="/terms-of-service" element={<TermsOfService />} />
-               
+               <Route path="/upload-data" element={<DataUploader />} />
                
               </Routes>
             </main>
