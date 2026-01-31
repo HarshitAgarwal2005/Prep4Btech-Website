c5a7dc37739f9ddd58e1f9529c97c21805e3ca56
Harshit Agarwal <harshitagarwal25807@gmail.com>
2026-01-31 14:49:29 +0530

    Updated contentData.ts

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -1046,31 +1046,7 @@ export const contentSubjects: ContentSubject[] = [
     icon: '📚'
   },
 
-  // ECE Subjects
-  // ECE - SEM 3
-  // {
-  //   id: 'signals-s3-ece',
-  //   name: 'Signals and Systems',
-  //   code: 'EC301',
-  //   year: 2,
-  //   semester: 3,
-  //   branch: 'ECE',
-  //   description: 'Signal processing fundamentals',
-  //   icon: '📡'
-  // },
-  // {
-  //   id: 'analog-s3-ece',
-  //   name: 'Analog Electronics', 
-  //   code: 'EC302',
-  //   year: 2,
-  //   semester: 3,
-  //   branch: 'ECE',
-  //   description: 'Analog circuit design and analysis',
-  //   icon: '📻'
-  // },
-
-  // EE Subjects
-  // EE - Semester 3
+ 
   {
     id: 'ae-s3-ee',
     name: 'Advance Mathematics',
